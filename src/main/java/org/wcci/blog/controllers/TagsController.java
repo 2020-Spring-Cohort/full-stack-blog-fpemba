7275559dc64a541ab0afc5e5ccb98dade1926d41
fpemba <fatuma.pemba@yahoo.com>
2020-02-22 11:48:40 -0500

    TagsController
    Tried to fix tags controller so that post method works, still needs more fixing, made changes to the index and post html files.

--- src/main/java/org/wcci/blog/controllers/TagsController.java
+++ src/main/java/org/wcci/blog/controllers/TagsController.java
@@ -2,7 +2,11 @@ package org.wcci.blog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.wcci.blog.models.Tags;
 
 @Controller
 @RequestMapping("tags")
@@ -16,5 +20,17 @@ public class TagsController {
     public String displayTags(Model model) {
         model.addAttribute("tags", "tag");
         return "tags";
+    }
+
+    @GetMapping("")
+    public String addTagForm(Model model) {
+        model.addAttribute("title", "Add a tag");
+        return "tags";
+    }
+
+    @PostMapping("submit")
+    public String processAddTagForm(@RequestParam("tagName") String tagName, @RequestParam("tagPost") String tagPost) {
+        new Tags(tagName, tagPost);
+        return "redirect:";
     }
 }
